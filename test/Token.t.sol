33e60791e00f2c75980e1e31d99025405938c7c2
SAsimRaza <asimraza2016@gmail.com>
2023-04-07 13:21:02 +0500

    feat: initial commit

--- test/Token.t.sol
+++ test/Token.t.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "../src/Box.sol";
+import "../src/Token.sol";
 
-contract BoxTest is Test {
-    Box t;
+contract TokenTest is Test {
+    Token t;
 
-    // function setUp() public {
-    //     t = new Token();
-    // }
+    function setUp() public {
+        t = new Token();
+    }
 
-    // function testName() public {
-    //     assertEq(t.name(), "Token");
-    // }
+    function testName() public {
+        assertEq(t.name(), "Token");
+    }
 }
